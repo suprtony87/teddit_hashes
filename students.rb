12a994d4ff32ffcda0696383f7675a60b2ee7620
Brooks Swinnerton <bswinnerton@gmail.com>
2015-03-21 13:18:41 -0400

    Adding student folders

---
+++ students.rb
@@ -0,0 +1,28 @@
+full_names = [
+  'Jay Nappy',
+  'Nuala Gaffey',
+  'Dave Kilsheimer',
+  'Trent Kurek',
+  'Matt Zeltser',
+  'Lex Evan',
+  'Tony Cellante',
+  'Mehan Jayasuriya',
+  'Emil Skandul',
+  'Lauren Brandstein',
+  'Rohan Siddhanti',
+  'David Tu',
+  'Mollie Panzner',
+  'Kemeya Harper',
+  'James Clavelli',
+  'David Klein',
+  'Catie Brand',
+]
+
+def underscoreize(string)
+  string.downcase.gsub(' ', '_')
+end
+
+full_names.each do |name|
+  %x( mkdir #{underscoreize(name)} )
+  %x( touch #{underscoreize(name)}/.gitkeep )
+end
